20a0e639bb8803414d525349e0f12b1e63d78789
tliam1 <tliam@hawaii.edu>
2022-03-29 22:57:01 -0500

    Cat Classes have been created

---
+++ catClass.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file catClass.cpp
+/// @version 1.0
+///
+/// @author Liam Tapper <tliam@hawaii.edu>
+/// @date   29_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "catClass.h"
---
+++ catClass.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab_08d_animal_farm_1_to_clion - EE 205 - Spr 2022
+///
+/// @file catClass.h
+/// @version 1.0
+///
+/// @author Liam Tapper <tliam@hawaii.edu>
+/// @date   29_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CATCLASS_H
+#define EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CATCLASS_H
+
+
+class catClass {
+
+};
+
+
+#endif //EE205_LAB_08D_ANIMAL_FARM_1_TO_CLION_CATCLASS_H
