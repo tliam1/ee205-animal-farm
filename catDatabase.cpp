1883a965161f37c61f59213a456aaff926d67af0
tliam1 <tliam@hawaii.edu>
2022-04-05 23:04:07 -0500

    started linked list data validation process... this is going to be rough

--- catDatabase.cpp
+++ catDatabase.cpp
@@ -18,15 +18,47 @@ numCats numberOfCats = 0;
 void initializeDatabase(){
     //numberOfCats = 0;
     if (catDatabaseHeadPointer != nullptr){
-
+        std::cout << "Database still exists (Should end program)" << std::endl;
     }
 
 }
 
 bool securityCheckCat(const class catClass* catToCheck){
-    return 1; //true
+    if (catToCheck == nullptr){
+        std::cout << "There is no cat here..." << std::endl;
+        return false;//end program
+    }
+    if (!dataValidation()) {//data is not valid
+        std::cout << "There is no cat here" << std::endl;
+        return false;//end program
+    }
+    //while the cat does not equal the null pointer, start from head and move down the list
+    for(catClass* cat = catDatabaseHeadPointer; cat != nullptr; cat = cat->next){
+        if(cat == catToCheck){
+            return true; //we found our cat!!!
+        }
+    }
+
+    if (!dataValidation()) {//data is not valid
+        std::cout << "There is no cat here" << std::endl;
+        return false; //end program
+    }
+    return false; //we never found our cat :C //end program
 }
 
 bool dataValidation(){
-    return 1; //true
+
+    int validatedCats = 0;
+    //while the cat does not equal the null pointer, start from head and move down the list
+    for(catClass* cat = catDatabaseHeadPointer; cat != nullptr; cat = cat->next){
+        //we want to validate the cat (using class validate function)
+        //check if the cat has a dup name
+    }
+
+    //if number of cats is  not equal to the number of validated cats then return false
+
+    //else return true
+    return true;
+
+    return true;
 }
